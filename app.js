e197e45411440bd4c18ed6dcd69875a242730464
Dongmoon Min <ehdans06@naver.com>
2017-11-12 17:02:53 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require ('express');
+var fs = require ('fs');
+
+var app = express ();
+
+app.get ('/', function (req, res) {
+    fs.readFile ('./views/index.html', function (error, data) {
+        if (error) {
+            console.log (error);
+        }
+        else {
+            res.writeHead (200, {'Content-Type':'text/html; charset=utf-8'});
+            res.end (data);
+        }
+    })
+});
+
+app.listen (8080, function () {
+    console.log ('Connected');
+});
